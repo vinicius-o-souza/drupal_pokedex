12f0c35576d2efed409dd53eb8541656064de1c8
Vinícius de Oliveira Souza <viniciuso970@gmail.com>
2024-08-15 15:00:35 -0300

    Styling filters in Pokemon page and export configurations.

--- web/themes/custom/pokemon/js/details.js
+++ web/themes/custom/pokemon/js/details.js
@@ -7,20 +7,40 @@
   'use strict';
 
   Drupal.behaviors.pokemonDetails = {
-    attach: function (context, settings) {      
+    attach: function (context, settings) {
 
-      once('pokemonDetails', 'details.js-form-wrapper', context).forEach(function (element) {
-        const details = document.querySelectorAll('details.form-wrapper');
-        details.forEach(detail => {
-          const summary = detail.querySelector('summary');
-          summary.addEventListener('click', function(event) {
-            const icon = this.querySelector('.bi');
-            icon.classList.toggle('bi-chevron-down');
-            icon.classList.toggle('bi-chevron-up');
+      const detailsElements = once('pokemonDetails', '.js-form-type-details details.form-wrapper', context);
+      detailsElements.forEach(detailsElement => {
+
+        // Add an event listener to the summary element
+        const summaryElement = detailsElement.querySelector('summary');
+        summaryElement.addEventListener('click', event => {
+          const iconElement = event.currentTarget.querySelector('.bi');
+          iconElement.classList.toggle('bi-chevron-down');
+          iconElement.classList.toggle('bi-chevron-up');
+        });
+
+        // Add an event listener to the form wrapper legend elements
+        document.addEventListener('click', event => {
+          const formWrapperLegendElements = document.querySelectorAll('.js-form-type-details details.form-wrapper summary');
+  
+          // Close the form wrapper if the click is outside the form wrapper
+          formWrapperLegendElements.forEach(formWrapperLegendElement => {
+            if (!formWrapperLegendElement.contains(event.target)) {
+              
+              if (formWrapperLegendElement.closest('details.form-wrapper') !== event.target.closest('details.form-wrapper')) {
+                const detailsElement = formWrapperLegendElement.closest('details.form-wrapper');
+                if (detailsElement.open) {
+                  detailsElement.open = false;
+                  const iconElement = formWrapperLegendElement.querySelector('.bi');
+                  iconElement.classList.remove('bi-chevron-up');
+                  iconElement.classList.add('bi-chevron-down');
+                } 
+              }
+            }
           });
         });
       });
-
     }
   };
 
