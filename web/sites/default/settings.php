6813087519c008424cb873a063c883ff6d1902d9
Vinícius de Oliveira Souza <viniciuso970@gmail.com>
2024-08-26 20:26:19 -0300

    Installing redis module.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -7,6 +7,8 @@ $settings['container_yamls'][] = __DIR__ . '/services.yml';
 
 $settings['config_sync_directory'] = '../config/sync';
 
+$settings['state_cache'] = TRUE;
+
 /**
  * Include the Pantheon-specific settings file.
  *
@@ -33,4 +35,76 @@ include __DIR__ . "/settings.pantheon.php";
 $local_settings = __DIR__ . "/settings.local.php";
 if (file_exists($local_settings)) {
   include $local_settings;
+}
+
+if (
+   isset($_ENV['PANTHEON_ENVIRONMENT']) 
+   && !\Drupal\Core\Installer\InstallerKernel::installationAttempted()
+   && extension_loaded('redis')
+) {
+  // Set Redis as the default backend for any cache bin not otherwise specified.
+  $settings['cache']['default'] = 'cache.backend.redis';
+ 
+  //phpredis is built into the Pantheon application container.
+  $settings['redis.connection']['interface'] = 'PhpRedis';
+ 
+  // These are dynamic variables handled by Pantheon.
+  $settings['redis.connection']['host'] = $_ENV['CACHE_HOST'];
+  $settings['redis.connection']['port'] = $_ENV['CACHE_PORT'];
+  $settings['redis.connection']['password'] = $_ENV['CACHE_PASSWORD'];
+ 
+  $settings['redis_compress_length'] = 100;
+  $settings['redis_compress_level'] = 1;
+ 
+  $settings['cache_prefix']['default'] = 'pantheon-redis';
+ 
+  $settings['cache']['bins']['form'] = 'cache.backend.database'; // Use the database for forms
+ 
+  // Apply changes to the container configuration to make better use of Redis.
+  // This includes using Redis for the lock and flood control systems, as well
+  // as the cache tag checksum. Alternatively, copy the contents of that file
+  // to your project-specific services.yml file, modify as appropriate, and
+  // remove this line.
+  $settings['container_yamls'][] = 'modules/contrib/redis/example.services.yml';
+ 
+  // Allow the services to work before the Redis module itself is enabled.
+  $settings['container_yamls'][] = 'modules/contrib/redis/redis.services.yml';
+ 
+  // Manually add the classloader path, this is required for the container
+  // cache bin definition below.
+  $class_loader->addPsr4('Drupal\\redis\\', 'modules/contrib/redis/src');
+ 
+  // Use redis for container cache.
+  // The container cache is used to load the container definition itself, and
+  // thus any configuration stored in the container itself is not available
+  // yet. These lines force the container cache to use Redis rather than the
+  // default SQL cache.
+  $settings['bootstrap_container_definition'] = [
+    'parameters' => [],
+    'services' => [
+      'redis.factory' => [
+        'class' => 'Drupal\redis\ClientFactory',
+      ],
+      'cache.backend.redis' => [
+        'class' => 'Drupal\redis\Cache\CacheBackendFactory',
+        'arguments' => [
+          '@redis.factory',
+          '@cache_tags_provider.container',
+          '@serialization.phpserialize',
+        ],
+      ],
+      'cache.container' => [
+        'class' => '\Drupal\redis\Cache\PhpRedis',
+        'factory' => ['@cache.backend.redis', 'get'],
+        'arguments' => ['container'],
+      ],
+      'cache_tags_provider.container' => [
+        'class' => 'Drupal\redis\Cache\RedisCacheTagsChecksum',
+        'arguments' => ['@redis.factory'],
+      ],
+      'serialization.phpserialize' => [
+        'class' => 'Drupal\Component\Serialization\PhpSerialize',
+      ],
+    ],
+  ];
 }
